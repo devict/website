860b3fd4f5dac5a65d9d5f1928878b0f578cc9d5
cdamyx <chris@amyx.engineering>
2023-10-31 06:59:47 -0500

    Update projects with single repo and issues below

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -25,20 +25,24 @@ export default async function Home() {
     <>
       <h1 class="text-4xl font-bold mb-4">Contribute to devICT</h1>
       <ul class="list-none">
-        {issues.map((issue) => {
+        {issues.map((issue, index) => {
           const [orgName, repoName] = issue.repository_url.split("/").slice(-2);
           const repoPath = `${orgName}/${repoName}`;
           const repoUrl = `https://github.com/${repoPath}`;
-          return (
+          return index === 0 || index > 0 && issue.repository_url != issues[index - 1].repository_url ? (
             <li class="my-1">
               <span class="font-bold underline hover:text-gray-600">
                 <a href={repoUrl}>{repoPath}</a>:{" "}
               </span>
-              <span>
-                <a href={issue.html_url} class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">{issue.title}</a>
-              </span>
+              <ul>{issues.map((issue) => {
+                  if((issue.html_url).includes(repoPath)){
+                    return (<li><span>
+                      <a href={issue.html_url} class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600">{issue.title}</a>
+                    </span></li>)
+                  }
+                  })}</ul>
             </li>
-          );
+          ) : ''
         })}
       </ul>
     </>
