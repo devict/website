18b2b414fb6f8fd7902d69966c9311d8db903882
cdamyx <chris@amyx.engineering>
2023-11-02 18:16:58 -0500

    Remove blank lines

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -40,7 +40,6 @@ export default async function Home() {
   return (
     <div class="container mx-auto px-4">
       <h1 class="text-4xl font-bold mb-4">Contribute to devICT</h1>
-
       <div class="grid grid-cols-4 gap-4">
         <div class="col-span-3">
           <IssuesList {...issuesListProps} />
@@ -53,6 +52,5 @@ export default async function Home() {
         </div>
       </div>
     </div>
-
   );
 }
