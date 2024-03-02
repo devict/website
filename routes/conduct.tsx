7021087b57083730225c2aa34bb3f17426bfbb34
Seth Etter <mail@sethetter.com>
2024-03-01 22:19:31 -0600

    conduct page placeholder

---
+++ routes/conduct.tsx
@@ -0,0 +1,9 @@
+export default function Conduct() {
+  return (
+    <div class="container mx-auto my-8">
+      <h1 class="text-4xl">Code of Conduct</h1>
+      <hr class="mx-auto my-8" />
+      <p>TODO!</p>
+    </div>
+  );
+}
