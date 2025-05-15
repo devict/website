3c1e367191f5d6323388b9654acd28025cc18cd9
Seth Etter <mail@sethetter.com>
2025-05-15 15:49:47 -0500

    Update one-time donation link

--- routes/support.tsx
+++ routes/support.tsx
@@ -39,32 +39,15 @@ export default function Support() {
             <h2 class="text-2xl my-4">One-time Donations</h2>
             <p>
               If you want to help but do not want to set up a recurring donation
-              you can give a single amount using PayPal.
+              you can give a single amount using{" "}
+              <a
+                href="https://www.paypal.com/ncp/payment/F4GL83F4M34QL"
+                title="Donate to devICT"
+              >
+                PayPal or Venmo
+              </a>
+              .
             </p>
-            <form
-              action="https://www.paypal.com/cgi-bin/webscr"
-              method="post"
-              target="_top"
-            >
-              <input type="hidden" name="cmd" value="_s-xclick" />
-              <input
-                type="hidden"
-                name="hosted_button_id"
-                value="TV7Y785BF7V4G"
-              />
-              <input
-                type="image"
-                src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif"
-                name="submit"
-                alt="PayPal - The safer, easier way to pay online!"
-              />
-              <img
-                alt=""
-                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                width="1"
-                height="1"
-              />
-            </form>
           </div>
         </div>
       </div>
